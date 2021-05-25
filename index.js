1f8a0950724e7a3744b58ec1a295ded462fc2a9f
lunisel <selene942008@hotmail.com>
2021-05-25 20:19:29 +0200

    Updated version without javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+const changeH1Content = function(){
+    let h1Changed = document.getElementById("pageTitleJava")
+    console.log(h1Changed)
+    h1Changed.innerText = "Your New Shop"
+}
+
+changeH1Content()
