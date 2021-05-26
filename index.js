026183f55085ab9eccc3049cb3c95600ce5101b9
lunisel <selene942008@hotmail.com>
2021-05-26 17:04:41 +0200

    Ex till 9 done

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const changeH1Content = function(){
     let h1Changed = document.getElementById("pageTitleJava")
-    console.log(h1Changed)
     h1Changed.innerText = "Your New Shop"
 }
 
-changeH1Content()
+const changeH1ContentBack = function(){
+    let h1ChangedBack = document.getElementById("pageTitleJava")
+    h1ChangedBack.innerText = "Luna's Shop"
+}
+
+const changeBackground = function(){
+    let newBackground = document.getElementsByTagName("body")[0]
+    newBackground.classList.toggle("active")
+}
+
+const changeFooter = function(){
+    document.getElementById("pOfFooter").innerHTML = "Copyright© 2021 Luna's Shop - Unicorn Street, 37 Neverland"
+}
+
+const addClassToA = function(){
+    
+} 
